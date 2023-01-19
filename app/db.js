498f574d6cdc519bd2deb3831369bd15a60fcb71
Piyush Bhandari <piyush.bhandari@outlook.com>
2023-01-19 14:04:19 +0000

    minor change to connection string

--- app/db.js
+++ app/db.js
@@ -6,6 +6,6 @@ const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'sharkdata';
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin?directConnection=true`;
 
 mongoose.connect(url, {useNewUrlParser: true});
